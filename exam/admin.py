237b4ff28a25108092d8295e951e9cad6408c9ac
alikarimi80 <alikarimi1380@yahoo.com>
2021-03-13 20:01:22 +0330

    exam taghriban completed

--- exam/admin.py
+++ exam/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from exam.models import *
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
 admin.site.register(Exam)
-# admin.site.register(Examiner)
 admin.site.register(ExamResult)
-# admin.site.register(QuestionResult)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        ChoiceInline,
+    ]
+admin.site.register(Question,QuestionAdmin)
